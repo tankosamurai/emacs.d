d988f829d66524e62a470200ed0033bb46e28327
tankosamurai <tankosamurai@outook.com>
2015-08-03 14:30:01 +0000

    update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -1,1 +1,11 @@
-export ALTERNATE_EDITOR=""
+if [ -d $HOME/.cask/bin ]; then
+    export PATH="$HOME/.cask/bin:$PATH"
+fi
+
+if [ ! -z $TERM ]; then
+    export TERM="xterm-256color"
+fi
+
+if [ ! -z $ALTERNATE_EDITOR ]; then
+    export ALTERNATE_EDITOR=""
+fi
